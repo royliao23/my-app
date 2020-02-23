ca8ad46798d647bb5ecaa353f4e3fd41f1166568
roy liao <roy_y_liao@hotmail.com>
2020-02-24 10:45:29 +1100

    Updates

---
+++ precache-manifest.e297e1edd817878707ccab9038a70d25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cdf1645132cce079359553f4b101f27",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "82e75e5cbf626999fa0b",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/my-app/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-app/static/js/2.b6fabe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82e75e5cbf626999fa0b",
+    "url": "/my-app/static/js/main.6c32197b.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
